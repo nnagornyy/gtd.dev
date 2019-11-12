d504d74f31915be462ffbb642a72d53225e54d03
nnagornyy <n.nagornyy@gmail.com>
2019-11-12 18:38:48 +0300

    вынес в языковый файл название

--- install/index.php
+++ install/index.php
@@ -1,10 +1,15 @@
 <?php
-
+use Bitrix\Main\Localization\Loc;
 
 class gtd_dev extends CModule{
 
     public $MODULE_ID = "gtd.dev";
-    public $MODULE_NAME = "Модуль для полезных фичей разработки";
+    public $MODULE_NAME = "";
+
+    public function __construct()
+    {
+        $this->MODULE_NAME = Loc::getMessage("GTD_DEV_MODULE_NAME");
+    }
 
     public function DoInstall()
     {
